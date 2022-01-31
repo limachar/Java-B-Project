0c64472ccd16b4d8ca1e92961fd0535f7711f519
linn.mann@gmail.com <linn.mann@gmail.com>
2022-01-31 14:26:30 +0100

    First draft of Course class and Teacher class

---
+++ src/main/java/sti/example/projectjavablinn/Entity/Course.java
@@ -0,0 +1,2 @@
+package sti.example.projectjavablinn.Entity;public class Course {
+}
---
+++ src/main/java/sti/example/projectjavablinn/Entity/Teacher.java
@@ -0,0 +1,34 @@
+package sti.example.projectjavablinn.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_teacher")
+@Getter
+@Setter
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Long number;
+    private int hourlyPay;
+
+    @OneToMany
+    @JoinColumn(name = "courses_id")
+    private Course courses;
+
+
+}
