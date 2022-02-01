6a7778b55c1d32166f7a63ec8f5163e95141243d
linn.mann@gmail.com <linn.mann@gmail.com>
2022-02-01 10:14:06 +0100

    removed the word public

--- src/main/java/sti/example/projectjavablinn/Repository/StudentRepository.java
+++ src/main/java/sti/example/projectjavablinn/Repository/StudentRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    public Optional<Student> findByNumber(Long number);
+    Optional<Student> findByNumber(Long number);
 
 }
